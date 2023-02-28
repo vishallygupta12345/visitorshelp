497d2269ec7a854fb317e7a648384063c2206df7
vishallygupta12345 <vishallygupta123@gmail.com>
2023-02-28 20:25:10 +0530

    remove config

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export default {
+    EMAIL: 'vishallygupta123@gmail.com',
+    PASSWORD: 'nfbzwtvrhimubnrl',
+    JWT_SECRET: "4Z4Cj8TvSoED2YWCxzSXknZSFSTz2WgO9mIHUCxlbYM=",
+    ATLAS_URI: "mongodb+srv://vishally:vishally@qr-database.43tkpux.mongodb.net/?retryWrites=true&w=majority"
+};
