8cf9582d45b1a7a4376ce420761a0ffff26c0e6c
vishallygupta12345 <vishallygupta123@gmail.com>
2023-02-28 21:17:17 +0530

    mail commit

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export default {
     EMAIL: 'vishallygupta123@gmail.com',
-    PASSWORD: 'nfbzwtvrhimubnrl',
+    PASSWORD: 'wgtxazpxnxdxaxlt',
     JWT_SECRET: "4Z4Cj8TvSoED2YWCxzSXknZSFSTz2WgO9mIHUCxlbYM=",
     ATLAS_URI: "mongodb+srv://vishally:vishally@qr-database.43tkpux.mongodb.net/?retryWrites=true&w=majority"
 };
