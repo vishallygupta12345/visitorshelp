25653e9f129b98baf2a9e8affd405311310043f1
vishallygupta12345 <vishallygupta123@gmail.com>
2023-03-01 16:38:47 +0530

    new2

--- backend-test/config.js
+++ backend-test/config.js
@@ -1,6 +1,4 @@
 export default {
-    EMAIL: 'vishallygupta123@gmail.com',
-    PASSWORD: 'cvhukbdnqsicrcuj',
     JWT_SECRET: "xhZ0DMEPg/aIdLteoi8nM4XevmB5bYYRueSWpZ9I37Y=",
     ATLAS_URI: "mongodb+srv://vishally:vishally@qr-database.43tkpux.mongodb.net/?retryWrites=true&w=majority"
 };
--- frontend-test/src/middleware/auth.js
+++ frontend-test/src/middleware/auth.js
@@ -1,15 +1,6 @@
 import { Navigate } from "react-router-dom";
-import { useGStore } from "../store/storeGuestName";
 import {useAuthStore} from '../store/storeTestKey.js'
 
-// to protect otp page from unauthorized users
-export const ProtectRoute = ({ children }) => {
-    const guestname = useGStore.getState().auth.guestname;
-    if(!guestname){
-        return <Navigate to={'/'} replace={true}></Navigate>
-    }
-    return children;
-}
 
 // to protect scanning page from unauthorized users
 export const Authorize = ({ children }) => {
